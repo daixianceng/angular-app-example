3db2f21d5522527d769cd9c55777a2cd9caf51e5
Cosmo <daixianceng@gmail.com>
2018-02-03 15:28:30 +0800

    优化 `routerClick` 事件

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -1,5 +1,4 @@
 import { Component, EventEmitter, Output } from '@angular/core';
-import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
@@ -8,23 +7,9 @@ import { Router, NavigationEnd } from '@angular/router';
 })
 export class NavComponent {
 
-  private shouldEmit = false;
+  @Output() routerClick: EventEmitter<any> = new EventEmitter();
 
-  @Output() navigate: EventEmitter<any> = new EventEmitter();
-
-  constructor(
-    private router: Router
-  ) {
-    router.events
-      .filter(() => this.shouldEmit)
-      .filter((event) => event instanceof NavigationEnd)
-      .subscribe((event: NavigationEnd) => {
-        this.navigate.emit(event);
-        this.shouldEmit = false;
-      });
-  }
-
-  onLinkClick(): void {
-    this.shouldEmit = true;
+  onRouterClick(): void {
+    this.routerClick.emit();
   }
 }
