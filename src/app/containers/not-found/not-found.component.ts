af604bdefb92fef3e98b10c6209f47cf2aefe0b9
Cosmo <daixianceng@gmail.com>
2018-02-04 12:21:25 +0800

    设计404页

--- src/app/containers/not-found/not-found.component.ts
+++ src/app/containers/not-found/not-found.component.ts
@@ -1,6 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { Location } from '@angular/common';
+
+import { StyleService } from 'app/services';
 
 @Component({
-  templateUrl: './not-found.component.html'
+  templateUrl: './not-found.component.html',
+  styleUrls: ['./not-found.component.css']
 })
-export class NotFoundComponent { }
+export class NotFoundComponent implements OnDestroy {
+
+  constructor(
+    private location: Location,
+    private styleService: StyleService
+  ) {
+    styleService.addStyle('notFoundStyle', require('./style.css'));
+  }
+
+  ngOnDestroy(): void {
+    this.styleService.removeStyle('notFoundStyle');
+  }
+
+  back() {
+    this.location.back();
+  }
+}
--- src/app/containers/not-found/not-found.module.ts
+++ src/app/containers/not-found/not-found.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { NotFoundComponent } from './not-found.component';
 
 @NgModule({
   imports: [
+    RouterModule,
+    FlexLayoutModule
   ],
   declarations: [
     NotFoundComponent
