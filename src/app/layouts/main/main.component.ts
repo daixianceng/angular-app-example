bb6d3057847b7427eb7421549988320f70713e95
Cosmo <daixianceng@gmail.com>
2018-03-24 18:34:55 +0800

    feat(animation): add "fadeIn" animation when route changes

--- src/app/layouts/main/main.component.ts
+++ src/app/layouts/main/main.component.ts
@@ -1,5 +1,6 @@
 import { Component, Inject } from '@angular/core';
 import { Router, NavigationEnd, ActivatedRoute, Data } from '@angular/router';
+import { trigger, style, animate, transition } from '@angular/animations';
 
 import { LOGIN_URL } from 'app/common';
 import { User } from 'app/models';
@@ -9,7 +10,15 @@ import { environment } from 'environments/environment';
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
-  styleUrls: ['./main.component.scss']
+  styleUrls: ['./main.component.scss'],
+  animations: [
+    trigger('fadeIn', [
+      transition('* => *', [
+        style({ opacity: 0 }),
+        animate('200ms ease-in-out')
+      ])
+    ])
+  ]
 })
 export class MainComponent {
 
@@ -17,6 +26,7 @@ export class MainComponent {
   title: string;
   primaryNav = true;
   user: User;
+  childComponent: any;
 
   constructor(
     private router: Router,
@@ -28,6 +38,9 @@ export class MainComponent {
       .filter((event) => event instanceof NavigationEnd)
       .map(() => this.route.firstChild)
       .filter((childRoute) => childRoute !== null)
+      .do((childRoute) => {
+        this.childComponent = childRoute.component;
+      })
       .switchMap((childRoute) => childRoute.data)
       .subscribe((data: Data) => {
         this.title = data.title;
